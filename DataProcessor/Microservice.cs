78f9865cd0a12a9efb9f4c12cec5699cde061c9a
Uladzislau Darazhei <yladzislay@gmail.com>
2023-12-06 10:10:51 +0300

    Initialize DataProcess microservice.

---
+++ DataProcessor/Microservice.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ;
+
+namespace DataProcessor;
+
+public class Microservice(ILogger<Microservice> logger, RabbitMqClient rabbitMqClient) : IHostedService
+{
+    private int ReceivedMessagesCount { get; set; }
+    public int GetReceivedMessagesCount() => ReceivedMessagesCount;
+    
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DataProcessorMicroservice is starting.");
+        rabbitMqClient.SubscribeForMessages(ProcessMessage);
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DataProcessorMicroservice is stopping.");
+        return Task.CompletedTask;
+    }
+
+    private void ProcessMessage(string message)
+    {
+        logger.LogInformation("Processing message: {Message}", message);
+        ReceivedMessagesCount++;
+    }
+}
--- development-plan.md
+++ development-plan.md
@@ -1,18 +1,24 @@
 ## Development Plan:
+
 ### Passed:
+
 1. Initialize XmlParser microservice.
 2. Improve XmlParser with parsing of RapidControlStatus section.
 3. Initialize RabbitMQ for microservices communication:
+
    1. Define RabbitMQ project with RabbitMqClient.
    2. Add message publish and messages subscriber methods.
    3. Use RabbitMqClient at XmlParser microservice.
    4. Refactoring: Define tests units.
 
+4. Initialize DataProcessor microservice.
+
 ### Next:
-1. Initialize DataProcessor microservice.
-2. Define a full cycle integration test.
-3. Add a full cycle integration test.
-4. Define and extract configurations.
+
+1. Define a full cycle integration test. 
+2. Add a full cycle integration test. 
+3. Define and extract configurations.
 
 ### Additional:
+
 1. Collect messages if connection lost.

dfd2d4c5622212d472e227e13e283d79291e7059
Uladzislau Darazhei <yladzislay@gmail.com>
2023-12-07 19:09:53 +0300

    Cover the AutoMapper with a test of the detected type cast error.

--- Tests/DatabaseUnits.cs
+++ Tests/DatabaseUnits.cs
@@ -28,14 +28,29 @@ namespace Tests
             var json = File.ReadAllText(jsonFilePath);
             var instrumentStatus = JsonSerializer.Deserialize<InstrumentStatus>(json);
             Assert.NotNull(instrumentStatus);
+            
             var instrumentStatusEntity = _mapper.Map<InstrumentStatusEntity>(instrumentStatus);
             Assert.NotNull(instrumentStatusEntity);
             Assert.Equal(instrumentStatus.PackageID, instrumentStatusEntity.PackageID);
+            
             var moduleState = instrumentStatus.DeviceStatusList[0].RapidControlStatus.CombinedStatus.ModuleState;
             Assert.NotNull(moduleState);
+            
             var mappedModuleState = instrumentStatusEntity.DeviceStatusList[0].RapidControlStatus.CombinedStatus.ModuleState;
             Assert.NotNull(mappedModuleState);
             Assert.Equal(moduleState, mappedModuleState);
+            
+            var firstDeviceStatus = instrumentStatusEntity.DeviceStatusList[0];
+            Assert.NotNull(firstDeviceStatus.RapidControlStatus);
+            Assert.IsType<CombinedSamplerStatusEntity>(firstDeviceStatus.RapidControlStatus.CombinedStatus);
+
+            var secondDeviceStatus = instrumentStatusEntity.DeviceStatusList[1];
+            Assert.NotNull(secondDeviceStatus.RapidControlStatus);
+            Assert.IsType<CombinedPumpStatusEntity>(secondDeviceStatus.RapidControlStatus.CombinedStatus);
+
+            var thirdDeviceStatus = instrumentStatusEntity.DeviceStatusList[2];
+            Assert.NotNull(thirdDeviceStatus.RapidControlStatus);
+            Assert.IsType<CombinedOvenStatusEntity>(thirdDeviceStatus.RapidControlStatus.CombinedStatus);
         }
     }
 }

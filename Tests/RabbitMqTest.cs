ea877eff8fb20bb8a2987af838cbe8a143e2c1e2
Uladzislau Darazhei <yladzislay@gmail.com>
2023-12-11 11:50:40 +0300

    Refactoring: to file-scoped namespace.

--- Tests/RabbitMqTest.cs
+++ Tests/RabbitMqTest.cs
@@ -3,47 +3,46 @@ using Microsoft.Extensions.DependencyInjection;
 using RabbitMQ;
 using Xunit;
 
-namespace Tests
+namespace Tests;
+
+public class RabbitMqTest : IAsyncLifetime
 {
-    public class RabbitMqTest : IAsyncLifetime
+    private IServiceProvider? _serviceProvider;
+    private RabbitMqClient? RabbitMqClient { get; set; }
+
+    public async Task InitializeAsync()
     {
-        private IServiceProvider? _serviceProvider;
-        private RabbitMqClient? RabbitMqClient { get; set; }
+        var configuration = new ConfigurationBuilder().Build();
+        _serviceProvider = new ServiceCollection()
+            .AddSingleton<IConfiguration>(configuration)
+            .AddSingleton<RabbitMqClient>()
+            .BuildServiceProvider();
 
-        public async Task InitializeAsync()
-        {
-            var configuration = new ConfigurationBuilder().Build();
-            _serviceProvider = new ServiceCollection()
-                .AddSingleton<IConfiguration>(configuration)
-                .AddSingleton<RabbitMqClient>()
-                .BuildServiceProvider();
+        RabbitMqClient = _serviceProvider.GetRequiredService<RabbitMqClient>();
+        await Task.CompletedTask;
+    }
 
-            RabbitMqClient = _serviceProvider.GetRequiredService<RabbitMqClient>();
-            await Task.CompletedTask;
-        }
+    public async Task DisposeAsync()
+    {
+        (_serviceProvider as IDisposable)?.Dispose();
+        await Task.CompletedTask;
+    }
 
-        public async Task DisposeAsync()
-        {
-            (_serviceProvider as IDisposable)?.Dispose();
-            await Task.CompletedTask;
-        }
+    [Fact]
+    public void ShouldBeSuccessfullyConnected()
+    {
+        Assert.True(RabbitMqClient?.IsConnected());
+    }
 
-        [Fact]
-        public void ShouldBeSuccessfullyConnected()
-        {
-            Assert.True(RabbitMqClient?.IsConnected());
-        }
-
-        [Fact]
-        public async Task MessageShouldBeSuccessfullyPublished()
-        {
-            const string message = "test_message";
-            Assert.True(RabbitMqClient?.IsConnected());
-            string? receivedMessage = null;
-            RabbitMqClient?.SubscribeForMessages(msg => receivedMessage = msg);
-            RabbitMqClient?.PublishMessage(message);
-            await Task.Delay(100);
-            Assert.Equal(message, receivedMessage);
-        }
+    [Fact]
+    public async Task MessageShouldBeSuccessfullyPublished()
+    {
+        const string message = "test_message";
+        Assert.True(RabbitMqClient?.IsConnected());
+        string? receivedMessage = null;
+        RabbitMqClient?.SubscribeForMessages(msg => receivedMessage = msg);
+        RabbitMqClient?.PublishMessage(message);
+        await Task.Delay(100);
+        Assert.Equal(message, receivedMessage);
     }
 }

71e93f78590c69109d875188c923494e83b99935
Uladzislau Darazhei <yladzislay@gmail.com>
2023-12-13 14:53:17 +0300

    Cover DataProcessor.Microservice process with logging.

--- DataProcessor/Microservice.cs
+++ DataProcessor/Microservice.cs
@@ -12,21 +12,22 @@ public class Microservice
         ILogger<Microservice> logger,
         RabbitMqClient rabbitMqClient, 
         Repository repository
-    ) : IHostedService
+    ) 
+    : IHostedService
 {
     private int ReceivedMessagesCount { get; set; }
     public int GetReceivedMessagesCount() => ReceivedMessagesCount;
     
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        logger.LogInformation("DataProcessorMicroservice is starting.");
         rabbitMqClient.SubscribeForMessages(ProcessMessageAsync);
+        logger.LogInformation("Microservice has been started.");
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        logger.LogInformation("DataProcessorMicroservice is stopping.");
+        logger.LogInformation("Microservice has been stopped.");
         return Task.CompletedTask;
     }
 
@@ -36,5 +37,18 @@ public class Microservice
         var instrumentStatus = JsonSerializer.Deserialize<InstrumentStatus>(message);
         if (instrumentStatus == null) return;
         await repository.SaveOrUpdateInstrumentStatusAsync(instrumentStatus);
+        
+        var loggingInformationMessage = $"Incoming instrument-status has been successfully deserialized " +
+                                 $"& saved to database with next module states:{Environment.NewLine}";
+
+        foreach (var device in instrumentStatus.DeviceStatuses)
+        {
+            loggingInformationMessage += $"[{instrumentStatus.PackageID}]" +
+                                  $"[{device.ModuleCategoryID}]" +
+                                  $"[{device.RapidControlStatus.CombinedStatus?.ModuleState}]" +
+                                  $"{Environment.NewLine}";
+        }
+
+        logger.LogInformation(loggingInformationMessage);
     }
 }
